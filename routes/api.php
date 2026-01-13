07d253fa3c2921d026255b947149de1bd5b2b436
Muhammad Hanif <hanifeij@yahoo.co.id>
2026-01-13 21:19:03 +0700

    feat: update login

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use App\Http\Controllers\PomodoroController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-// Public routes
-Route::post('/login', [AuthController::class, 'login']);
+// Public routes (with web middleware for session support)
+Route::middleware('web')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 // Protected routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['web', 'auth'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
 
